897da798fd8f8ef4a068af628633ff35fe073eba
Rorro2009 <108434614+Rorro2009@users.noreply.github.com>
2022-10-03 17:57:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var nombre = "Rodrigo";
+console.log(nombre);
+
+var apellido = "Martinez";
+console.log(apellido);
+
+var nombre_completo = nombre.concat(apellido);
+console.log(nombre_completo);
+
+var primero = 10;
+
+var segundo = 5;
+
+var suma = primero.suma(segundo);
+console.log(suma);
